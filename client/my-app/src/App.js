33f542d59fd57e31d1b573e29ecc28c130d2a224
izazasif <41232484+izazasif@users.noreply.github.com>
2020-11-07 23:58:10 +0600

    frist version

---
+++ client/my-app/src/App.js
@@ -0,0 +1,76 @@
+import React,{useState,useEffect} from 'react';
+import './App.css';
+import Axios from "axios";
+
+function App() {
+  var [movieName,setMovieName] = useState("");
+  var [review,setReview] = useState("");
+  var [movieReviewList,setMoviewList]=useState([]);
+
+  var [newReview,setNewReview]=useState("");
+
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/api/get").then((response)=>{
+      setMoviewList(response.data);
+    });
+  });
+
+  var submitReview = ()=>{
+   Axios.post("http://localhost:3001/api/insert",{ 
+     movieName: movieName,
+     movieReview:review,
+   });
+  //  then(()=> {
+  //    alert("successfull")
+  //  });
+  setMoviewList([...movieReviewList,{ movieName: movieName,movieReview: review},]);
+  };
+  
+  var deleteReview =(movie)=>{
+    Axios.delete("http://localhost:3001/api/delete/${movie}");
+
+  };
+
+  var updateReview =(movie)=>{
+    Axios.put("http://localhost:3001/api/update",{
+      movieName:movie,
+      movieReview: newReview,
+    });
+    setNewReview("")
+  };
+  return (
+    <div className="App">
+      <h1>Movie Review Site</h1>
+      <div className="form">
+        <label>Movie Name</label>
+        <input type="text" name="movieName" onChange={(e)=>{
+          setMovieName(e.target.value);
+        }}/>
+        <label>Movie Review</label>
+        <input type="text" name="review" onChange={(e) =>{
+          setReview(e.target.value);
+        } }/>
+        <button onClick={submitReview}>Submit</button>
+
+        {
+          movieReviewList.map((val) => {
+            return (
+              <div className="card">
+              <h1>
+                MovieName : {val.movieName}
+                </h1>
+                <p>MovieReview : {val.movieReview}</p>
+
+                <button onClick={()=>{deleteReview(val.movieName)}}>Delete</button>
+                <input type="text" id="updateInput" onChange={(e)=>{setNewReview(e.target.value)}}/>
+                <button onClick={()=> {updateReview(val.movieName)}}>update</button>
+                </div>
+            );
+          })}
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/server/index.js
@@ -0,0 +1,85 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var mysql = require('mysql');
+var bodyParser 	= require('body-parser');
+const { request, response } = require("express");
+
+var db = mysql.createPool({
+     host:'localhost',
+    user : 'root',
+    password : '',
+    database : 'cruddatabase',
+
+})
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/get",function(request,response){
+
+    var sql = "select * from movie_review";
+    db.query(sql,function(err,result){
+    if(err)
+    {
+        response.send(err);  
+    }
+    else
+    response.send(result);
+        
+    });
+
+})
+
+app.post("/api/insert",function(request,response){
+    var movieName = request.body.movieName;
+    var movieReview= request.body.movieReview;
+    var sql = "INSERT INTO movie_review (movieName,movieReview) VALUES (?,?)";
+    db.query(sql,[movieName,movieReview],function(err,result){
+    if(err)
+    {
+        response.send(err);  
+    }
+    else
+    response.send("SUCCESS");
+        
+    });
+   
+});
+app.delete("/api/delete/:movieName",function(request,response){
+    var name = request.params.movieName;
+    var sql = "DELETE FROM movie_review where movieName= ? ";
+    db.query(sql,name,function(err,result){
+        if(err)
+        {
+            console.log(err);  
+        }
+        else
+        console.log("SUCCESS");
+            
+        });
+
+});
+
+app.put("/api/update",function(request,response){
+    var name = request.body.movieName;
+    var reviw= request.body.movieReview;
+    var sql = "UPDATE movie_review SET movieReview=? where movieName= ?";
+    db.query(sql,[name,reviw],function(err,result){
+        if(err)
+        {
+            console.log(err);  
+        }
+        else
+        response.send("SUCCESS");
+            
+        });
+
+});
+
+app.listen(3001,function()
+{
+   console.log("ServerStart at 3001");
+})
